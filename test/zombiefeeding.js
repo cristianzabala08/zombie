e337b548c9a98a9474a0706b9a8da5f51a27537c
czabala98 <61285007+czabala98@users.noreply.github.com>
2024-03-13 21:15:35 -0400

    ready

--- test/zombiefeeding.js
+++ test/zombiefeeding.js
@@ -1,3 +1,7 @@
+
+const ganache = require('ganache');
+const Web3 = require('web3');
+const [web3, provider] = require('tronbox-web3')(new Web3(Web3.givenProvider), ganache.provider());
 const SimpleStorage = artifacts.require("zombiefeeding.sol");
 
 contract("zombiefeeding", (accounts) => {
@@ -13,3 +17,5 @@ contract("zombiefeeding", (accounts) => {
       })
   });
 });
+
+        
